6fa93a469222429848b8610aa030fc4b011835bf
rajasri9949 <rajasrichalamala@gmail.com>
2025-04-15 11:30:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 customhost = "database-1.cw1u0oou06dk.us-east-1.rds.amazonaws.com"
 customuser = "admin"
 custompass = "Rajasri123"
-customdb = "database-1"
+customdb = "database1"
 custombucket = "projectbucket156"
 customregion = "us-east-1"
